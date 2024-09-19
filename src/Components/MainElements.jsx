fd4eb7b5c405042b4f0c3409a36618edcd6b1f24
Farothtc <mvkirimi@gmail.com>
2024-09-19 13:56:34 +0300

    Props are placed

--- src/Components/MainElements.jsx
+++ src/Components/MainElements.jsx
@@ -7,23 +7,21 @@ export function MainElements(props) {
 
       <div className="col-md-9 pe-5">
         <img src="./pin.png" />
-        <a href="">
+        <a href={props.e.map}>
           <small>View on Google Maps</small>
         </a>
         <div className="col-md-12">
-          <h1>Mount Fuji</h1>
+          <h1>{props.e.title}</h1>
         </div>
         <div className="col-md-12">
           <p>
-            <strong>12 Jan, 2021 - 24 Jan, 2021</strong>
+            <strong>
+              {props.e.date.start} - {props.e.date.end}
+            </strong>
           </p>
         </div>
         <div className="col-md-12">
-          <p>
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+          <p>{props.e.description}</p>
         </div>
       </div>
       <hr />
